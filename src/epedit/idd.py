72520786d0dff37d4fa14e5915bec8e58ef19db4
Rubicell <chp.rubicell@gmail.com>
2025-08-31 19:20:45 +0900

    added extensible fields

--- src/epedit/idd.py
+++ src/epedit/idd.py
@@ -17,7 +17,7 @@ class FieldProps:
 class ExtensibleProps:
     start_idx: int  # start index of the extensible fields
     size: int  # size of the extensible fields
-    key_re_str: List[str]  # RegExp pattern for extensible field search
+    key_re_strs: List[str]  # RegExp pattern for extensible field search
     fieldnames: List[Tuple[str, str]] # prefix, suffix -> (prefix)(n)(suffix)
 
 @dataclass
@@ -28,16 +28,16 @@ class ClassProps:
     last_default_field_idx: int = -1  # index of the last default field
     extensible: Optional[ExtensibleProps] = None
 
-def parse_IDDClass_string(class_str: str, verbose: bool = False) -> ClassProps:
+def parse_IDDClass_str(class_str: str, verbose: bool = False) -> ClassProps:
 
     #? get top-level class info
-    class_info_string_match = re.search(r'[^\s,]+,(?:\r\n|\r|\n)(?: *\\.*(?:\r\n|\r|\n))+', class_str)  # Version, \~~, \~~
-    if class_info_string_match:
-        class_info_string = class_info_string_match.group()
+    class_info_str_match = re.search(r'[^\s,]+,(?:\r\n|\r|\n)(?: *\\.*(?:\r\n|\r|\n))+', class_str)  # Version, \~~, \~~
+    if class_info_str_match:
+        class_info_str = class_info_str_match.group()
     else:
         raise ValueError('No class info found!')
     
-    classname_match = re.search(r'([^\s,]+),', class_info_string)
+    classname_match = re.search(r'([^\s,]+),', class_info_str)
     if classname_match:
         classname = classname_match.group(1)
     else:
@@ -46,7 +46,17 @@ def parse_IDDClass_string(class_str: str, verbose: bool = False) -> ClassProps:
     #? create classProps instance for this class
     classprops = ClassProps(
         classname = classname,
-        fields=SimpleNamespace()
+        fields = SimpleNamespace()
     )
+
+    #? check extensible
+    extensible_match = re.search(r'\\extensible:(\d+)', class_info_str)
+    if extensible_match:
+        classprops.extensible = ExtensibleProps(
+            start_idx   = -1,  # update during the field parsing
+            size        = int(extensible_match.group(1)),
+            key_re_strs = [],
+            fieldnames  = []
+        )
 
     return classprops
