3d06adbc682c01b2b7ca58c6c66dd4d4600c5277
Rubicell <chp.rubicell@gmail.com>
2026-07-24 14:38:09 +0900

    added more test cases

--- test/test_utils.py
+++ test/test_utils.py
@@ -9,13 +9,15 @@ def run_test():
     print(repr(utils.test_to_lower("ssd!@SFD")))
     print(repr(utils.test_to_upper("ssd!@SFD")))
     print(repr(utils.test_trim_string("  A$ test\n\t")))
-
+    print()
     print(repr(utils.test_has_prefix("test", "te")))
     print(repr(utils.test_has_prefix("test", "st")))
-
+    print(repr(utils.test_has_prefix("test", "")))
+    print()
     print(repr(utils.test_has_suffix("test", "te")))
     print(repr(utils.test_has_suffix("test", "st")))
-
+    print(repr(utils.test_has_suffix("test", "")))
+    print()
     print(repr(utils.test_cut_prefix("test", "te")))
     print(repr(utils.test_cut_prefix("test", "st")))
 
