6daffddc4306161d7942f2ac0fa33d12b285a129
Rubicell <chp.rubicell@gmail.com>
2026-08-03 02:05:45 +0900

    improved test functions

--- tests/test_idd.py
+++ tests/test_idd.py
@@ -1,16 +1,15 @@
 # -*- coding: utf-8 -*-
 import sys
 from pathlib import Path
-sys.path.append(str(Path("../src/epedit").resolve()))
+sys.path.append(str(Path("../src").resolve()))
 
-from idd import IDD, test_find_field_index, test_get_field_name
+from epedit.idd import IDD, test_find_field_index, test_get_field_name
 
 def test_idd_parser(file_path: str):
     idd = IDD.from_file(file_path)
 
     print()
-    print("====")
-    print("Parse test")
+    print("== Parse test ====")
     print(idd.num_classes)
     if idd.num_classes != 848:
         raise ValueError
@@ -20,21 +19,27 @@ def test_idd_parser(file_path: str):
     # for i in range(idd.num_classes):
         print(idd.get_class_name(i))
 
+
     print()
-    print("====")
-    print("Find field index test")
+    print("== Parsed field name test ====")
+    for field_name in idd._get_field_names("BuildingSurface:Detailed"):
+        print(field_name)
+
+    print()
+    print("== Find field index test ====")
     print(test_find_field_index(idd, "BuildingSurface:Detailed", "Zone Name"))  # 3
     print(test_find_field_index(idd, "BuildingSurface:Detailed", "Vertex 10 X-coordinate"))  # 38
     print(test_find_field_index(idd, "BuildingSurface:Detailed", "Vertex 30 X-coordinate"))  # 98
     print(test_find_field_index(idd, "BuildingSurface:Detailed", "Vertex 30 Z-coordinate"))  # 100
 
     print()
-    print("====")
-    print("Get field name test")
+    print("== Get field name test ====")
     print(test_get_field_name(idd, "BuildingSurface:Detailed", 3, True))
     print(test_get_field_name(idd, "BuildingSurface:Detailed", 38, True))
     print(test_get_field_name(idd, "BuildingSurface:Detailed", 98, True))
     print(test_get_field_name(idd, "BuildingSurface:Detailed", 100, True))
 
+    print()
+
 if __name__ == "__main__":
-    test_idd_parser("../dev/idds/V24-2-0-Energy+.idd")
+    test_idd_parser("./idds/V24-2-0-Energy+.idd")
--- tests/test_idf.py
+++ tests/test_idf.py
@@ -9,5 +9,24 @@ def test_idf():
     idd = IDD.from_file("./idds/V24-2-0-Energy+.idd")
     idf = IDF.from_file(idd, "./files/RefBldgMediumOfficeNew2004_Chicago.idf")
 
+    print()
+    print("== Parse test ====")
+    obj = idf.get_objects("BuildingSurface:Detailed".lower())[0]
+    print(obj.class_name)
+
+    print()
+    print("== Get value test ====")
+    print(obj[2], "==", obj["Construction Name"])
+    print(type(obj["Number of Vertices"]), repr(obj["Number of Vertices"]))
+    print(type(obj["Vertex 1 X-coordinate"]), repr(obj["Vertex 1 X-coordinate"]))
+    print(type(obj["Vertex 10 X-coordinate"]), repr(obj["Vertex 10 X-coordinate"]))
+
+    print()
+    print("== Set value test ====")
+    obj["Vertex 10 X-coordinate"] = 10
+    print(type(obj["Vertex 10 X-coordinate"]), repr(obj["Vertex 10 X-coordinate"]))
+
+    print()
+
 if __name__ == "__main__":
     test_idf()
