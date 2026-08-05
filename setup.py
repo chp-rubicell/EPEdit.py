65f6712474a8aaf1c6c6e656ad55cb4163efab8d
Rubicell <chp.rubicell@gmail.com>
2026-08-05 10:13:13 +0900

    updated project info

--- setup.py
+++ setup.py
@@ -1,17 +1,34 @@
-import sys
+import os
 from setuptools import setup, Extension
 from Cython.Build import cythonize
 
-if len(sys.argv) == 1:
-    sys.argv.extend(["build_ext", "--inplace"])
+# import sys
+# if len(sys.argv) == 1:
+#     sys.argv.extend(["build_ext", "--inplace"])
 # same as executing `python setup.py build_ext --inplace`
 
+# OS specific options
+if os.name == "nt":
+    # Windows
+    extra_compile_args = [
+        "/O2",        # optimization level
+        "/std:c++14"  # force C++14 (MSVC)
+    ]
+else:
+    # Linux & macOS
+    extra_compile_args = [
+        "-O3",        # optimization level
+        "-std=c++14"  # force C++14 (GCC)
+    ]
+
 # Define extensions separately to give the C++ file special instructions
-ext_modules = [
-    Extension("epedit.utils", sources=["src/epedit/utils.pyx"], language="c++"),
-    Extension("epedit.lexer", sources=["src/epedit/lexer.pyx"], language="c++"),
-    Extension("epedit.idd", sources=["src/epedit/idd.pyx"], language="c++"),
-    Extension("epedit.idf", sources=["src/epedit/idf.pyx"], language="c++"),
+extensions = [
+    Extension(
+        name               =  "epedit.*",
+        sources            = ["src/epedit/*.pyx"],
+        extra_compile_args = extra_compile_args,
+        language           = "c++",
+    ),
 ]
 
 setup(
@@ -22,5 +39,14 @@ setup(
     package_data={
         "epedit": ["*.pyi", "py.typed"],
     },
-    ext_modules=cythonize(ext_modules, language_level=3, annotate=True),
+    ext_modules=cythonize(
+        extensions,
+        compiler_directives = {
+            'language_level': "3",  # force Python 3
+            'boundscheck': False,   #
+            'wraparound': False,
+        },
+        annotate=False,
+    ),
+    zip_safe=False,
 )
