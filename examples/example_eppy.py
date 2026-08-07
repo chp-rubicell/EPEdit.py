1d25bcadf19db9233466f5343f9b4309a9df5219
Rubicell <chp.rubicell@gmail.com>
2026-08-07 17:18:39 +0900

    added examples for eppy mode

--- examples/example_eppy.py
+++ examples/example_eppy.py
@@ -1,63 +1,68 @@
 # -*- coding: utf-8 -*-
 import sys
 from pathlib import Path
-sys.path.insert(0, str(Path("../src").resolve()))
+sys.path.append(str(Path("../src").resolve()))
 
-from epedit.eppy import IDF
-# from eppy.modeleditor import IDF
+# from epedit.eppy import IDF
+from eppy.modeleditor import IDF
 
-def example_eppy():
-    IDF.setiddname(Path("./idds/V24-2-0-Energy+.idd"))
-    idf = IDF(Path("./files/RefBldgMediumOfficeNew2004_Chicago.idf"))
 
-    print()
-    print("== Parse test ====")
-    obj = idf.idfobjects["BuildingSurface:Detailed".upper()][0]
-    print(obj)
+# * Open IDF files
 
-    print()
-    print("== Get value test ====")
-    print(type(obj.Number_of_Vertices), repr(obj.Number_of_Vertices))
-    print(type(obj.Vertex_1_Xcoordinate), repr(obj.Vertex_1_Xcoordinate))
-    print(type(obj.Vertex_10_Xcoordinate), repr(obj.Vertex_10_Xcoordinate))
+# Load the EnergyPlus IDD schema.
+IDF.setiddname("idds/V24-2-0-Energy+.idd")
 
-    print()
-    print("== Set value test ====")
-    print(type(obj.Vertex_10_Xcoordinate), repr(obj.Vertex_10_Xcoordinate))
+# Load an existing IDF file.
+idf = IDF("files/input.idf")
 
-    print()
-    print("== Get object test ====")
-    print(idf.getobject("BuildingSurface:Detailed", "Core_bot_ZN_5_Wall_East"))
-    print(idf.getobject("BuildingSurface:Detailed", "asdf"))
-    '''
-    print()
-    print("== Add object test ====")
-    new_objs = []
-    for i in range(3):
-        obj = idf.add_object("BuildingSurface:Detailed")
-        new_objs.append(obj)
-        print(obj.obj_idx, ":", obj)
+# * Iterate over objects
 
-    print()
-    print("== Default values test ====")
-    obj = idf.add_object("OutputControl:Files", default_values=False)
-    print(repr(obj["Output CSV"]))
-    obj = idf.add_object("OutputControl:Files", default_values=True)
-    print(repr(obj["Output CSV"]))
-    obj = idf.add_object("OutputControl:Files", initial_values={"Output CSV": True})
-    print(repr(obj["Output CSV"]))
+# Iterate over objects using idf.idfobjects[classname]
+for sch_type_limits in idf.idfobjects["ScheduleTypeLimits"]:
+    print(sch_type_limits.Name)
 
-    print()
-    print("== Remove object test ====")
-    print(len(idf.get_objects("BuildingSurface:Detailed")))
-    for obj in new_objs:
-        print(["Failed","Removed"][idf.remove_object(obj)])
-    print(len(idf.get_objects("BuildingSurface:Detailed")))
-    print(f"{idf.remove_all_objects('BuildingSurface:Detailed')} objects removed.")
-    print(idf.get_objects("BuildingSurface:Detailed"))
+# * Find and update objects
 
-    print()
-    '''
+# Find an object by class name and object name.
+building = idf.getobject("Building", "My Building")
 
-if __name__ == "__main__":
-    example_eppy()
+# Update fields by their IDD field names.
+building.North_Axis = 0.0
+building.Terrain = "City"
+
+print(building)
+
+# * Add new objects
+
+# Add a new object with initial field values.
+obj = idf.newidfobject(
+    "RunPeriod",
+    Name                      = "New Annual Run",
+    Begin_Month               = 1,
+    Begin_Day_of_Month        = 1,
+    End_Month                 = 12,
+    End_Day_of_Month          = 31,
+    Day_of_Week_for_Start_Day = "Monday",
+)
+
+# Add a new object without default values.
+obj = idf.newidfobject(
+    "Material",
+    defaultvalues = False,
+    Name          = "New Insulation",
+    Thickness     = 0.05,
+    Conductivity  = 0.0314,
+    Density       = 265,
+    Specific_Heat = 836.8,
+)
+
+# * Remove objects
+
+# Remove an object
+obj = idf.getobject("RunPeriod", "annual")
+idf.removeidfobject(obj)
+
+# * Save IDF files
+
+# Save the modified IDF file.
+idf.save("files/output_eppy.idf")
